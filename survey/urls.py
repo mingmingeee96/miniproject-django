29dbd76e4672e31392caa25e9dcd48b860d31bd9
BE-02-KMJ <mjk5912@naver.com>
2024-04-15 16:24:05 +0900

    requirements 수정

--- survey/urls.py
+++ survey/urls.py
@@ -3,7 +3,7 @@ from .views import survey_view, question_view, result_view
 
 urlpatterns = [
     path('', survey_view, name='survey'),
-    path('survey/', question_view, name='question'),
-    # path('question/', question_view, name='question'),
+    # path('survey/', question_view, name='question'),
+    path('question/', question_view, name='question'),
     path('result/', result_view, name='result'),
 ]
